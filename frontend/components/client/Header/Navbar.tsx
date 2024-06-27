ff3eca037dd7932d91907a505b4124a636e4ab60
gyan-cell <jha07944@gmail.com>
2024-06-27 12:18:36 +0530

    NavBar And Stuff

--- frontend/components/client/Header/Navbar.tsx
+++ frontend/components/client/Header/Navbar.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import { NaveElements } from '../clientComponents'
+import '@/styles/header/navbar.scss'
 const Navbar = () => {
   return (
     <nav>
       <div id='logo'>
         <a href='/'>ChinSawMan.</a>
       </div>
+      <NaveElements />
     </nav>
   )
 }
---
+++ frontend/components/client/clientComponents.tsx
@@ -0,0 +1,24 @@
+"use client"
+import React from "react";
+import { FaBars } from "react-icons/fa6";
+import { TfiClose } from "react-icons/tfi";
+
+export const NaveElements = () => {
+  const [show, setShow] = React.useState(false);
+  return (
+    <div className="NaveElements"  >
+      <div className="Hamburger"  >
+        {
+          show
+            ? <i className="fa-solid fa-xmark text-3xl z-50" onClick={() => setShow(!show)}><TfiClose /></i>
+            : <i className="fa-solid fa-barsi text-3xl z-50" onClick={() => setShow(!show)}><FaBars /></i>
+        }
+      </div>
+      <ul className={show ? "show" : ""}  >
+        <li><a href='/'>Home</a></li>
+        <li><a href='/'>About</a></li>
+        <li><a href='/'>Blogs</a></li>
+      </ul>
+    </div>
+  )
+}
